de790ac6c07642c85bbc1c706e1feefc14d08f6e
Michael Prenez-Isbell <prenezisbell@gmail.com>
2023-10-11 15:10:14 -0400

    Initial Commit

---
+++ Mapkit Sean Allen/Mapkit_Sean_AllenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mapkit_Sean_AllenApp.swift
+//  Mapkit Sean Allen
+//
+//  Created by Michael Prenez-Isbell on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Mapkit_Sean_AllenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
